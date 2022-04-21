07889c2878c1bd6d077135fbd5606d588b3539cc
Vansh <vanshtandon1067@gmail.com>
2022-04-21 10:30:23 +0530

    Add welcome Route

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express=require("express")
 const app=express()
 
 
-
+app.use("/",(req,res,next)=>{
+        res.send("Welcome to ProxyCatcher")
+})
 app.listen(3000,()=>{
     console.log("Server created successfully")
 })
